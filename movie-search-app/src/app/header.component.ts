f635fa087ef17fe3250b78bb359586f6916731fe
Estefani <estefania@outlook.com>
2019-09-26 18:11:38 -0500

    a complete project in angular. It is about search movies and display the information

---
+++ movie-search-app/src/app/header.component.ts
@@ -0,0 +1,13 @@
+import {Component, NgModule} from '@angular/core'
+
+@Component({
+    selector:"app-header"
+    ,template:`
+        <span>header</span>
+    `
+})
+
+export class HeaderComponent
+{
+
+}
